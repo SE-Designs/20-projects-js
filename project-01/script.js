9cf7239c6fdb79807986f3c1a0a54a93c503bdec
SE-Designs <64894804+SE-Designs@users.noreply.github.com>
2022-08-15 16:27:32 +0600

    loader

--- project-01/script.js
+++ project-01/script.js
@@ -1,45 +1,66 @@
-const quoteText = document.querySelector('.quote');
-const author = document.querySelector('.author');
-const newQuoteBtn = document.querySelector('.new-quote');
-const twitter = document.querySelector('.twitter');
+const quoteText = document.querySelector(".quote");
+const author = document.querySelector(".author");
+const newQuoteBtn = document.querySelector(".new-quote");
+const twitter = document.querySelector(".twitter");
+const loader = document.querySelector("#loader");
+const quoteContainer = document.querySelector(".quote-container");
+
+// Show loader
+function showLoader() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loader
+function hideLoader() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
 
 let apiQuotes = [];
 
+// Show New Quote
 function newQuote() {
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    if (quote.text.length > 80) {
-        quoteText.classList.add('long-quote');
-    } else {
-        quoteText.classList.remove('long-quote');
-    }
-    
-    quoteText.textContent = quote.text;
-    
-    if (!quote.author) {
-        author.textContent = 'Unknown Author';
-    } else {
-        author.textContent = quote.author;
-    }
+  showLoader();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (quote.text.length > 80) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
+
+  if (!quote.author) {
+    author.textContent = "Unknown Author";
+  } else {
+    author.textContent = quote.author;
+  }
+  hideLoader();
 }
 
+// Get Quotes
 async function getQuotes() {
-    const config = 'https://type.fit/api/quotes';
+  showLoader();
+  const config = "https://type.fit/api/quotes";
 
-    try {
-        const response = await fetch(config);
-        apiQuotes = await response.json();
-        newQuote();
-    } catch (err) {
-        console.log('Error getting quotes');
-    }
+  try {
+    const response = await fetch(config);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (err) {
+    console.log("Error getting quotes");
+  }
 }
 
+// Tweet Quotes
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${author.textContent}`;
-    window.open(twitterUrl, '_blank');
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${author.textContent}`;
+  window.open(twitterUrl, "_blank");
 }
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitter.addEventListener("click", tweetQuote);
 
-newQuoteBtn.addEventListener('click', newQuote);
-twitter.addEventListener('click', tweetQuote);
-
+// On Load:
 getQuotes();
