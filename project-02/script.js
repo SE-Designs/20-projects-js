2d30db49d3fda26a73efcbf8a7cb1ce3bb01214e
SE-Designs <64894804+SE-Designs@users.noreply.github.com>
2022-08-17 16:29:04 +0600

    project-02, finished!

--- project-02/script.js
+++ project-02/script.js
@@ -1,69 +1,77 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loading-container");
 
-const portrait = document.getElementById("portrait");
-const landscape = document.getElementById("landscape");
-const squarish = document.getElementById("squarish");
-
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-function setNewState() {
-  let orient = document.querySelector(".active-btn").id;
+let count = 5;
+const apiKey = "BuK2UoRbuxadweVUE5GZ7FcRvgHndhlDVo9zzMvxpSw";
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-  const count = 10;
-  const apiKey = "BuK2UoRbuxadweVUE5GZ7FcRvgHndhlDVo9zzMvxpSw";
-  const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&orientation=${orient}`;
-
-  // Display Photos
-  function displayPhotos() {
-    setNewState();
-    photosArray.forEach((photo) => {
-      // LINK:
-      const item = document.createElement("a");
-      item.setAttribute("href", photo.links.html);
-      item.setAttribute("target", "_blank");
-      // IMAGE:
-      const img = document.createElement("img");
-      img.setAttribute("src", photo.urls.regular);
-      img.setAttribute("alt", photo.alt_description);
-      img.setAttribute("title", photo.alt_description);
-      // INHERIT:
-      item.appendChild(img);
-      imageContainer.appendChild(item);
-    });
-  }
-
-  // Get Photos
-  async function getPhotos() {
-    try {
-      const resp = await fetch(apiURL);
-      photosArray = await resp.json();
-      displayPhotos();
-    } catch (err) {}
-  }
-
-  getPhotos();
+// Image Loading
+function imageLoading() {
+  setTimeout(() => {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+      count = 2;
+      ready = true;
+      loader.style.display = "none";
+      console.log("ready: " + ready);
+    }
+  }, 1000);
 }
-window.onload = (event) => {
-  portrait.classList.add("active-btn");
-};
 
-// Event Listeners
-portrait.addEventListener("click", () => {
-  portrait.classList.add("active-btn");
-  landscape.classList.remove("active-btn");
-  squarish.classList.remove("active-btn");
+function setAttribute(el, att) {
+  for (const key in att) {
+    el.setAttribute(key, att[key]);
+  }
+}
+
+// Display Photos
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log("totalImages: " + totalImages);
+  photosArray.forEach((photo) => {
+    // LINK:
+    const item = document.createElement("a");
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target", "_blank");
+    // IMAGE:
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
+    // EVENT:
+    img.addEventListener("load", imageLoading());
+
+    // INHERIT:
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+// Get Photos
+async function getPhotos() {
+  try {
+    const resp = await fetch(apiURL);
+    photosArray = await resp.json();
+    displayPhotos();
+  } catch (err) {}
+}
+
+// Scroll event
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
 });
 
-landscape.addEventListener("click", () => {
-  landscape.classList.add("active-btn");
-  portrait.classList.remove("active-btn");
-  squarish.classList.remove("active-btn");
-});
-
-squarish.addEventListener("click", () => {
-  squarish.classList.add("active-btn");
-  landscape.classList.remove("active-btn");
-  portrait.classList.remove("active-btn");
-});
+getPhotos();
