937ca0ea0052dfb22da181a4113f5366f6ec557c
SE-Designs <64894804+SE-Designs@users.noreply.github.com>
2022-08-23 00:08:54 +0600

    working with js

--- project-05/script.js
+++ project-05/script.js
@@ -10,3 +10,62 @@
 //     });
 //   });
 // });
+
+hash = [
+  "#hero-section",
+  "#advantages-section",
+  "#about-section",
+  "#partners-section",
+  "#tokenomics-section",
+  "#contact-section",
+];
+
+let index = 0;
+const sideTags = document.querySelectorAll("#sidetag");
+
+// SCROLLBAR
+const scrollBar = document.querySelector("#scrollbar");
+
+// ON SCROLL
+window.onwheel = (e) => {
+  let windowHeight = window.innerHeight * index;
+  let scrollBarY = Math.round(
+    (windowHeight / document.body.offsetHeight) * 100
+  );
+  if (e.deltaY >= 0) {
+    // Scroll Down:
+    if (index == hash.length - 1) {
+      index = hash.length - 1;
+    } else {
+      index++;
+    }
+  } else {
+    // Scroll Up:
+    if (index == 0) {
+      index = 0;
+    } else {
+      index--;
+    }
+  }
+  setActive();
+  scrollTo(hash[index]);
+  console.log(scrollBarY);
+  scrollBar.style.top =
+    Math.round(scrollBarY * (window.innerHeight / 100)) + "px";
+};
+
+// SET ACTIVE
+function setActive() {
+  sideTags.forEach((el, id) => {
+    if (id == index) {
+      el.classList.add("sideactive");
+    } else {
+      el.classList.remove("sideactive");
+    }
+  });
+}
+
+// SCROLL TO ANCHOR
+function scrollTo(hash) {
+  location.hash = hash;
+}
